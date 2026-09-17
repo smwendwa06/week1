a5bc05460e33c3c1739a765343a07355ade71d83
Sanaipei Angel <asanaipei1574@stu.riarauniversity.ac.ke>
2026-09-17 14:45:44 +0300

    Initial commit: Hello World program

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+* My first Java program
+* Angel
+* 17th September 2026
+*/
+public class HelloWorld {
+public static void main(String[] args) {
+System.out.println("Hello, World!");
+System.out.println("My name is Angel");
+System.out.println("I am learning Java at Riara University");
+System.out.println("Today's date is 17th September 2026");
+}
+}
